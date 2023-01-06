f84cdcb85ef0172ee235b5c4117f50719bae4f59
BlowTorchhh <hanifkamil76@gmail.com>
2023-01-06 10:41:52 +0700

    perbaikan status pengeluaran

--- resources/views/pengeluaran/data.blade.php
+++ resources/views/pengeluaran/data.blade.php
@@ -29,9 +29,14 @@
     <div class="animated fadeIn">
 
         @if (session('status'))
-            <div class="alert alert-success">
-               {{ session('status') }}
+        <div class="col-sm-12">
+            <div class="alert  alert-success alert-dismissible fade show" role="alert">
+                <span class="badge badge-pill badge-success"></span> {{ session('status') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+        </div>
         @endif
 
         <div class="card">
