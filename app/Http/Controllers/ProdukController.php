231c6965fc7f8158bdb22f447539e28201e24fa8
RizalLiswanto <isaldiliswan12@gmail.com>
2023-01-11 15:05:48 +0700

    perbikanURL

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,7 +6,7 @@ use App\Models\kategori;
 use App\Models\produk;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use PhpParser\Node\Stmt\TryCatch;
 
 class ProdukController extends Controller
 {
@@ -23,16 +23,22 @@ class ProdukController extends Controller
     }
     public function addProcess(Request $request)
     {
-        DB::table('produk')->insert([
-            'nama_produk' => $request->nama_produk,
-            'kode'=> $request->kode,
-            'kategori_id'=> $request->kategori_id,
-            'merk'=>$request->merk,
-            'harga_jual'=>$request->harga_jual,
-            'harga_beli'=>$request->harga_beli,
-            'stok'=>$request->stok,
-          
-        ]);
+        try {
+            DB::table('produk')->insert([
+                'nama_produk' => $request->nama_produk,
+                'kode'=> $request->kode,
+                'kategori_id'=> $request->kategori_id,
+                'merk'=>$request->merk,
+                'harga_jual'=>$request->harga_jual,
+                'harga_beli'=>$request->harga_beli,
+                'stok'=>$request->stok,
+              
+            ]);
+           
+        } catch (\Throwable $th) {
+            return redirect('produk/produk')->with('error', 'produck sudah ada!');
+            
+        }
         return redirect('produk/produk')->with('status', 'produck Berhasil ditambah!');
     }
     public function edit($id)
--- routes/web.php
+++ routes/web.php
@@ -32,23 +32,7 @@ Route::group(['middleware' => ['auth']], function (){
     Route::group(['middleware' => ['\App\Http\Middleware\cekUserLogin:1']], function (){
         Route::resource('admin',HomeController::class);
         Route::get('home', [HomeController::class, 'index'])->name('home');
-    });
-    Route::group(['middleware' => ['\App\Http\Middleware\cekUserLogin:2']], function (){
-        Route::resource('user',HomeController::class);
-        Route::get('home', [HomeController::class, 'index'])->name('home');
-    });
-});
-
-
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-Route::get('password', [UserController::class, 'password'])->name('password');
-Route::post('password', [UserController::class, 'password_action'])->name('password.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
-
-Route::get('kategori/kategori', [KategoriController::class, 'index'])->name('kategori');
+        Route::get('kategori/kategori', [KategoriController::class, 'index'])->name('kategori');
 Route::get('kategori/kategori-add', [KategoriController::class, 'add'])->name('kategori-add');
 Route::post('kategoris', [KategoriController::class,'addProcess']);
 Route::get('kategori/kategori-edit/{id}',[KategoriController::class,'edit']);
@@ -75,6 +59,23 @@ Route::post('Barang-masuks', [BarangmasukController::class,'addProcess']);
 Route::get('Barang-masuk/Barang-masuk-edit/{id}',[BarangmasukController::class,'edit']);
 Route::patch('Barang-masuk/{id}', [BarangmasukController::class,'editProcess']);
 Route::delete('Barang-masuk/{id}',[BarangmasukController::class,'delete']);
+    });
+    Route::group(['middleware' => ['\App\Http\Middleware\cekUserLogin:2']], function (){
+        Route::resource('user',HomeController::class);
+        Route::get('home', [HomeController::class, 'index'])->name('home');
+    });
+});
+
+Route::get('/notfound', [UserController::class, 'index'])->name('notfound');
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+Route::get('password', [UserController::class, 'password'])->name('password');
+Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
+
 
 Route::get('laporan-kategori', [LaporanController::class, 'index'])->name('laporan.index');
 Route::get('laporan-kategori/pdf', [LaporanController::class, 'exportPDF'])->name('laporan.export');
